d2ee3ae01299950468f4cb58df4d021f88593d5e
DanyDolphin <danielcastillo59814@ciencias.unam.mx>
2022-07-13 21:14:04 -0500

    feature(ListCreateSongs): adding view to list and create songs

--- songs/views.py
+++ songs/views.py
@@ -3,6 +3,40 @@
 # Django
 from django.http import HttpResponse
 
-def index(request):
-    '''Songs' index view'''
-    return HttpResponse("Hello, world. You're at the songs index.")
+# Django REST Framework
+from rest_framework.views import APIView, status
+from rest_framework.response import Response
+
+# Models
+from songs.models import Song
+
+class ListCreateSongs(APIView):
+    '''View to list and create songs'''
+
+    def get(self, request):
+        '''Returns a list of all songs'''
+        queryset = Song.objects.all()
+        return Response(
+            [{
+                'name': song.name,
+                'author': song.author,
+                'genre': song.genre
+            } for song in queryset]
+        )
+
+    def post(self, request):
+        '''Creates a song'''
+        song = Song(
+            name=request.data['name'],
+            author=request.data['author'],
+            genre=request.data['genre']
+        )
+        song.save()
+        return Response(
+            {
+                'name': song.name,
+                'author': song.author,
+                'genre': song.genre
+            },
+            status.HTTP_201_CREATED
+        )
