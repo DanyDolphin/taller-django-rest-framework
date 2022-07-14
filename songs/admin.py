17821331cd9e15a81537541834545bee53101f58
DanyDolphin <danielcastillo59814@ciencias.unam.mx>
2022-07-13 21:03:34 -0500

    feature(admin): adding custom ModelAdmin classes for songs and playlists

--- songs/admin.py
+++ songs/admin.py
@@ -6,4 +6,7 @@ from django.contrib import admin
 # Models
 from songs.models import Song
 
-admin.site.register(Song)
+class SongAdmin(admin.ModelAdmin):
+    list_display = ('name', 'author', 'genre',)
+
+admin.site.register(Song, SongAdmin)
