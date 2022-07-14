9885653875944034aa6f995ef34879584d13cebd
DanyDolphin <danielcastillo59814@ciencias.unam.mx>
2022-07-13 22:59:14 -0500

    feature(PlaylistViewSet): adding playlist model viewset

--- playlists/urls.py
+++ playlists/urls.py
@@ -1,11 +1,22 @@
 '''Playlists' URLs'''
 
 # Django
-from django.urls import path
+from django.urls import path, include
+
+# Django REST Framework
+from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework.routers import SimpleRouter
 
 # Views
 from .views import *
 
+
+router = SimpleRouter()
+router.register('', PlaylistViewSet, basename='playlists')
+
+
 urlpatterns = [
-    path('', index, name='index'),
+    path('', include(router.urls))
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- playlists/views.py
+++ playlists/views.py
@@ -3,6 +3,28 @@
 # Django
 from django.http import HttpResponse
 
-def index(request):
-    '''Playlists' index view'''
-    return HttpResponse("Hello, world. You're at the playlists index.")
+# Django REST Framework
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.views import APIView, status
+from rest_framework.viewsets import ViewSet, ModelViewSet
+from rest_framework.response import Response
+from rest_framework.decorators import action
+
+# Models
+from playlists.models import Playlist
+
+# Serializers
+from playlists.serializers import PlaylistModelSerializer
+from songs.serializers import SongModelSerializer
+
+
+class PlaylistViewSet(ModelViewSet):
+    '''Playlist model viewset'''
+    queryset = Playlist.objects.all()
+    serializer_class = PlaylistModelSerializer
+
+    @action(detail=True)
+    def songs(self, request, pk=None):
+        playlist = self.get_object()
+        songs =playlist.songs
+        return Response(SongModelSerializer(songs, many=True).data)
